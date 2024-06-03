e48b49924a93013f367b333e8e15ca8d3385a489
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-06-03 20:12:56 +0200

    chore: implemented react query and loading screens

--- src/components/organisms/MapBoxMap/MapboxMap.tsx
+++ src/components/organisms/MapBoxMap/MapboxMap.tsx
@@ -3,13 +3,14 @@ import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import React, { useEffect, useRef, useState } from 'react'
 import Logo from '../../../assets/logo.png'
-import { IRegionProperties } from '../../../interfaces/IRegionProperties'
+import { IRegionData, IRegionProperties } from '../../../interfaces/IRegionProperties'
 import { fetchRegionDatetimes, fetchRegions } from '../../../services/mapService'
 import { addNavigationControls, getGlobeMap, loadStyles } from '../../../services/mapboxService'
 import { addPolygonLayer, addPredictionLayer, getBoundingBox } from '../../../services/predictionLayerService'
 import { addRegionLayer } from '../../../services/regionLayerService'
 import TopBanner from '../OEWHeader/OEWHeader'
 import './MapboxMap.css'
+import { useQuery } from '@tanstack/react-query'
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY!
 
@@ -17,18 +18,26 @@ const MapboxMap: React.FC = () => {
     const mapContainerRef = useRef<HTMLDivElement>(null)
     const [map, setMap] = useState<mapboxgl.Map | null>(null)
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const [regionId, setRegionId] = useState<number>(2)
+    // this causes the react query hook to refetch the data with the state is updated to null, so i left it emtpy
+    // const [regionId, setRegionId] = useState<number | null>(null)
+    const [regionId, setRegionId] = useState<number>()
 
     const openSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen)
     }
 
     const [regionProps, setRegionProps] = useState<undefined | IRegionProperties>(undefined)
+    const [regionData, setRegionData] = useState<undefined | IRegionData>(undefined)
 
     function handleDaySelect(days: number[]) {
         let toBeAddedDays = days
         if (!map || !map.getStyle()) {
             console.error('Map is not initialized or no style is loaded')
+            return
+        }
+
+        if (regionId === null) {
+            console.error('No region selected')
             return
         }
 
@@ -47,7 +56,7 @@ const MapboxMap: React.FC = () => {
             if (map.getLayer(`pred-${day}`)) {
                 map.setLayoutProperty(`pred-${day}`, 'visibility', 'visible')
             } else {
-                addPredictionLayer(map, day, regionId)
+                addPredictionLayer(map, day, regionId!)
             }
         })
     }
@@ -58,43 +67,135 @@ const MapboxMap: React.FC = () => {
         loadStyles(map)
         addNavigationControls(map)
 
-        fetchRegions()
-            .then((regions) => {
-                addRegionLayer(map, regions)
-                map.on('click', 'regions', (e) => {
-                    setRegionId(e.features![0].properties!.id)
-                    const regionName = e.features![0].properties!.name
-                    const regionSize = e.features![0].properties!.area_km2
-
-                    const aoiPolygon: Polygon = JSON.parse(e.features![0].properties!.polygon)
-
-                    fetchRegionDatetimes(regionId).then((regionDatetimes) => {
-                        map.setLayoutProperty('regions', 'visibility', 'none')
-                        map.fitBounds(getBoundingBox(aoiPolygon))
-
-                        addPolygonLayer(map, aoiPolygon)
-                        openSidebar()
-                        const jobs = regionDatetimes.map((regionDatetimes) => regionDatetimes.timestamp)
-                        const regionProps: IRegionProperties = {
-                            id: regionId,
-                            name: regionName,
-                            jobs: jobs,
-                            areaSize: regionSize,
-                        }
-                        setRegionProps(regionProps)
-                        addPredictionLayer(map, jobs[0], regionId)
-                    })
-                })
-            })
-            .catch((error) => console.error('Failed to load regions on map:', error))
-
         return () => map.remove()
     }, [])
 
+    async function fetchRegionsWithTimeout() {
+        //timout to simulate loading
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        return await fetchRegions()
+    }
+
+    const { isPending: fetchingAoiCenters, data } = useQuery({
+        queryKey: ['regions'],
+        // queryFn: fetchRegions,
+        queryFn: async () => await fetchRegionsWithTimeout(),
+    })
+
+    useEffect(() => {
+        if (data && map) {
+            loadRegionsOnMap(data)
+        }
+    }, [data, map])
+
+    //fails on hot reload, page reload works fine
+    function loadRegionsOnMap(regions: any) {
+        if (!map) {
+            console.error('Map is not initialized')
+            return
+        }
+        addRegionLayer(map, regions)
+
+        map.on('click', 'regions', (e) => {
+            const regionId = e.features![0].properties!.id
+            const regionName = e.features![0].properties!.name
+            const regionSize = e.features![0].properties!.area_km2
+            const regionPolygon: Polygon = JSON.parse(e.features![0].properties!.polygon)
+
+            const regionData: IRegionData = {
+                id: regionId,
+                name: regionName,
+                areaSize: regionSize,
+                polygon: regionPolygon,
+            }
+
+            //this causes the react query hook to refetch the data with the new regionId
+            setRegionId(e.features![0].properties!.id)
+            setRegionData(regionData)
+
+            return
+        })
+    }
+
+    async function fetchRegionDatetimesWithTimeout(regionId: number) {
+        if (!map) return []
+        if (!regionId) return []
+
+        //timout to simulate loading
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const regionDatetimes = await fetchRegionDatetimes(regionId)
+        const regionJobs = regionDatetimes.map((regionDatetimes) => regionDatetimes.timestamp)
+        addPredictionLayer(map, regionJobs[0], regionId)
+
+        map.setLayoutProperty('regions', 'visibility', 'none')
+        map.fitBounds(getBoundingBox(regionData?.polygon!))
+
+        addPolygonLayer(map, regionData?.polygon!)
+        openSidebar()
+
+        if (regionData && regionJobs) {
+            const newRegionProps: IRegionProperties = {
+                id: regionData.id,
+                name: regionData.name,
+                areaSize: regionData.areaSize,
+                jobs: regionJobs,
+            }
+            setRegionProps(newRegionProps)
+        }
+
+        return regionDatetimes
+    }
+
+    const { isPending: fetchingRegionData } = useQuery({
+        queryKey: [`regionData${regionId}`, regionId],
+        queryFn: async () => await fetchRegionDatetimesWithTimeout(regionId!),
+        enabled: regionId !== null,
+        refetchOnWindowFocus: false,
+    })
+
     return (
-        <div>
+        <div style={{ position: 'relative' }}>
+            {fetchingAoiCenters && (
+                <div
+                    style={{
+                        zIndex: 1000,
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Optional: This will add a semi-transparent black background
+                    }}
+                >
+                    <h1 style={{ color: 'white' }}>Loading...</h1>
+                </div>
+            )}
+
+            {fetchingRegionData && (
+                <div
+                    style={{
+                        zIndex: 1000,
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                    }}
+                >
+                    <h1 style={{ color: 'white' }}>Fetching Regions Data...</h1>
+                </div>
+            )}
+
             <TopBanner logo={Logo} isOpen={isSidebarOpen} regionProps={regionProps} handleSelectDays={handleDaySelect} map={map!}></TopBanner>
-            <div ref={mapContainerRef} className="map-container h-screen"></div>
+            <div ref={mapContainerRef} className="h-screen"></div>
         </div>
     )
 }
