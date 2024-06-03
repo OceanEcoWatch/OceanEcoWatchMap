59cc10092ecbe77f6ad75a9e6f00b2a43f4524b9
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-06-03 20:11:37 +0200

    chore: Add QueryClientProvider for React Query integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import MapboxMap from './components/organisms/MapBoxMap/MapboxMap'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
     return (
-        <div className="App">
-            <MapboxMap />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <MapboxMap />
+            </div>
+        </QueryClientProvider>
     )
 }
 
