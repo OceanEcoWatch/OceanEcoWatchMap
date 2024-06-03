7c9a8f16d784165793675d3104bbf971fc155288
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-06-03 20:12:34 +0200

    refactor: Add IRegionData interface

--- src/interfaces/IRegionProperties.tsx
+++ src/interfaces/IRegionProperties.tsx
@@ -1,6 +1,15 @@
+import { Polygon } from 'geojson'
+
 export interface IRegionProperties {
     id: number
     jobs: number[]
     name: string
     areaSize: number
+}
+
+export interface IRegionData {
+    id: number
+    name: string
+    areaSize: number
+    polygon: Polygon
 }
