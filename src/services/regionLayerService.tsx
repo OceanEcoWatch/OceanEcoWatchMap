2301564e97ea31b594becf06668e45e82c390aeb
emely3h <emely3h@gmail.com>
2024-06-04 22:54:29 +0200

    fix region bug + add clusteredLayer

--- src/services/regionLayerService.tsx
+++ src/services/regionLayerService.tsx
@@ -1,4 +1,4 @@
-import { FeatureCollection, GeoJsonProperties, Point } from 'geojson'
+import { Feature, FeatureCollection, GeoJsonProperties, Point } from 'geojson'
 import mapboxgl from 'mapbox-gl'
 
 function capitalizeFirstLetterOfEachWord(input: string): string {
@@ -55,4 +55,63 @@ export function addRegionLayer(map: mapboxgl.Map, regions: FeatureCollection<Poi
         },
     })
     addRegionPopup(map)
+}
+
+function addClusteredRegionPopup(map: mapboxgl.Map, content: string) {
+    const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+    })
+
+    map.on('mouseenter', 'clusteredRegions', (event) => {
+        map.getCanvas().style.cursor = 'pointer'
+
+        if (event.features![0].geometry.type === 'Point') {
+            const coordinates = event.features![0].geometry.coordinates.slice() ?? []
+            const name = event.features![0].properties?.name
+            const area = event.features![0].properties?.area_km2
+
+            while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360
+            }
+            popup.setLngLat([coordinates[0], coordinates[1]]).setHTML(content).addTo(map)
+        }
+    })
+    map.on('mouseleave', 'clusteredRegions', () => {
+        map.getCanvas().style.cursor = ''
+        popup.remove()
+    })
+}
+
+export function addClusteredRegions(map: mapboxgl.Map) {
+    const manillaBayPoint: Feature = {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [120.9749, 14.5547],
+        },
+        properties: {
+            title: 'Manila Bay',
+            description: 'Manila, Philippines',
+        },
+    }
+
+    map.addSource('clusteredRegions', {
+        type: 'geojson',
+        data: manillaBayPoint,
+    })
+
+    map.addLayer({
+        id: 'clusteredRegions',
+        type: 'circle',
+        source: 'clusteredRegions',
+        paint: {
+            'circle-color': '#ff0000',
+            'circle-radius': 9,
+            'circle-stroke-width': 6,
+            'circle-stroke-color': '#660000',
+        },
+    })
+    const popupContent: string = `<strong>Manilla Bay</strong><br>`
+    addClusteredRegionPopup(map, popupContent)
 }
