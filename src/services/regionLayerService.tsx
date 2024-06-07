3b4d872d19c76eb5c69a5037a8d5cb27e37d9f41
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-06-07 15:01:08 +0200

    cluster of aoi centers based on radius

--- src/services/regionLayerService.tsx
+++ src/services/regionLayerService.tsx
@@ -48,21 +48,54 @@ export function addAoiCentersLayer(
     map.addSource('aoi-centers', {
         type: 'geojson',
         data: regions,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 100,
+        clusterProperties: {
+            name: ['get', 'name'],
+            area_km2: ['get', 'area_km2'],
+        },
     })
 
     map.addLayer({
-        id: 'aoi-centers',
+        id: 'clusters',
         type: 'circle',
         source: 'aoi-centers',
+        filter: ['has', 'point_count'],
         paint: {
-            'circle-color': '#ff0000',
-            'circle-radius': 9,
-            'circle-stroke-width': 6,
-            'circle-stroke-color': '#660000',
+            'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
+            'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
         },
     })
+
+    map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'aoi-centers',
+        filter: ['has', 'point_count'],
+        layout: {
+            'text-field': ['get', 'point_count_abbreviated'],
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12,
+        },
+    })
+
+    map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'aoi-centers',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff',
+        },
+    })
+
     addRegionPopup(map)
-    map.on('click', 'aoi-centers', (e) => {
+
+    map.on('click', 'unclustered-point', (e) => {
         console.log('aoi-centers', e)
         const regionId = e.features![0].properties!.id
         const regionName = e.features![0].properties!.name
@@ -76,5 +109,22 @@ export function addAoiCentersLayer(
             polygon: regionPolygon,
         })
         setCurrentAoiId(regionId)
+    })
+
+    map.on('click', 'clusters', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+            layers: ['clusters'],
+        })
+        const clusterId = features[0].properties!.cluster_id
+        //@ts-ignore
+        map.getSource('aoi-centers')!.getClusterExpansionZoom(clusterId, (err, zoom) => {
+            if (err) return
+
+            map.easeTo({
+                //@ts-ignore
+                center: features[0].geometry.coordinates,
+                zoom: zoom,
+            })
+        })
     })
 }
