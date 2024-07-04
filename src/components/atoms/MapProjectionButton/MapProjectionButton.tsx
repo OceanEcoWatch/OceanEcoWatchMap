68b6154cb368043703082cb471f8969a3c4adce2
emely3h <emely3h@gmail.com>
2024-07-04 11:24:59 +0200

    add global colors, switch label

--- src/components/atoms/MapProjectionButton/MapProjectionButton.tsx
+++ src/components/atoms/MapProjectionButton/MapProjectionButton.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Toggle from 'react-toggle'
-import './MapProjectionButton.css'
 import { toggleMapProjection } from '../../../services/mapboxService'
+import './MapProjectionButton.css'
 
 interface MapProjectionButtonProps {
     map: mapboxgl.Map
 }
 
 const MapProjectionButton: React.FC<MapProjectionButtonProps> = ({ map }) => {
+    const [displayMap, setDisplayMap] = useState(true)
+
+    function onToggle() {
+        setDisplayMap(!displayMap)
+        toggleMapProjection(map)
+    }
+
+    function getLabel() {
+        if (displayMap) {
+            return 'Globe'
+        } else {
+            return 'Map'
+        }
+    }
+
     return (
         <label className="flex items-center space-x-2">
-            <Toggle defaultChecked={false} icons={false} onChange={() => toggleMapProjection(map)} />
-            <span>Globe</span>
+            {!displayMap && <span>Map</span>}
+            <Toggle defaultChecked={false} icons={false} onChange={onToggle} />
+            {displayMap && <span>Globe</span>}
         </label>
     )
 }
