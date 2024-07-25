eb1d913e95665dcd057df094fd78a4d9104bfc29
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-07-25 09:43:45 +0200

    Update src/components/organisms/usePredictionQuery.tsx

--- src/components/organisms/usePredictionQuery.tsx
+++ src/components/organisms/usePredictionQuery.tsx
@@ -4,7 +4,7 @@ import { fetchPredictions } from '../../services/mapService'
 export const usePredictionQuery = (timestampToFetch: number | undefined, currentAoiId: number | undefined) => {
     return useQuery({
         queryKey: ['prediction', timestampToFetch, currentAoiId],
-        queryFn: async () => await fetchPredictions(timestampToFetch!, currentAoiId!), //is das ok hier zu forcen?
+        queryFn: async () => await fetchPredictions(timestampToFetch!, currentAoiId!), 
         staleTime: Infinity, // Data never becomes stale
         refetchOnWindowFocus: false, // Do not refetch on window focus
         enabled: Boolean(timestampToFetch && currentAoiId),
